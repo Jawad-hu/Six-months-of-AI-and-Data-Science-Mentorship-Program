e68fab8526a7638997429e033c3d84aa9f0f0ac3
Jawad Hussain <114210079+Jawad-hu@users.noreply.github.com>
2025-04-12 14:04:36 +0500

    Add files via upload

---
+++ 02_variables.py
@@ -0,0 +1,17 @@
+A= 4 
+B= "Black Color"
+C = 2.5
+
+print (A,B,)
+
+#use of the variables and tips to use them
+    # 1 to perform calculations
+    # 2 To important the Data and Name that Data 
+    
+# Rules to create the variables 
+# 1. Variable name should not start with a number
+# 2. Variable name should not contain special characters except underscore(_)
+# 3 Do not use special characters like like @ * 
+# 4 do not use function which are bulit in python 
+# 5 Always use meaningful names for the variables
+# 6 Descreptive 
---
+++ 03_Operators.py
@@ -0,0 +1,21 @@
+a = 15 
+b = 4 
+c =20 
+
+print (a+b) # Addition
+print (b-a) # Subtraction
+print (c*b) # Multiplication
+print (a/b) # Division
+
+#print (a*b-c/a+8) # DMAS # PEMDAS # BODMAS 
+
+# Comparison Operators
+
+a = 14 
+b = 10
+c = 8 
+print(a<b)
+print (a>b)
+print (a==b)
+print (a!=b)
+# how to combine two are more operators in one line of code 
