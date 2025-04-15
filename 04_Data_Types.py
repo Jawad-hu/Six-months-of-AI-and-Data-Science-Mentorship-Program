7df01a3f3a4bc82e6958808e77c19b87776b1c92
Jawad Hussain <114210079+Jawad-hu@users.noreply.github.com>
2025-04-15 14:17:08 +0500

    Add files via upload

---
+++ 04_Data_Types.py
@@ -0,0 +1,13 @@
+x = 5                   #Integer
+y = 3.14                #Float
+z = "Hello, World!"  #String
+a = [1,2,3,4]     #List
+b = (1,2,3,4)  #Tuple
+
+c = ["Jawad", "Ali", "Sara"]  #List of strings
+d = { "Jawad": 1, "Ali": 2, "Sara": 3 }  #Dictionary
+
+e = {1,2,3,4}  #Set
+
+#How to Dictionary ?
+#Dictionary is a collection of key-value pairs.
